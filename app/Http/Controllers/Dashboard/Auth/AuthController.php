413f6a5d024eb271bd5a0e470c1f4c2e6b35afdc
AlaaMoShaat <alaamshaat445@gmail.com>
2025-01-07 15:26:11 +0200

    add to admin auth

---
+++ app/Http/Controllers/Dashboard/Auth/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller {
+    public function showLoginForm() {
+        return view('das.au')
+    }
+}
---
+++ routes/dashboard.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Dashboard\Auth\AuthController;
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ],
+    function () {
+        ################################ Auth #################################
+        Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+        Route::post('login', [AuthController::class, 'showLoginForm'])->name('login.post');
+    }
+);
